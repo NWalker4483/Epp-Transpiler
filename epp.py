045eb060ace88b94bff2babb0d6eaba378b70ad2
NWalker4483 <niwal7@morgan.edu>
2019-04-10 11:45:33 -0400

    Added comment remover

--- epp.py
+++ epp.py
@@ -17,6 +17,12 @@ for line in original_program:
     if line[0]=='#':
         includes.append(line)
         continue
+    # Remove comments 
+    if "//" in line:
+        line = line[:line.find('//')]
+        if len(line.strip()) == 0:
+            continue
+    # TODO: Add handler for string constants
     split_line = line
     for char in spec_char:
         split_line = re.sub('\{}'.format(char)," "+char+" ", split_line)
